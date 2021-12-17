8ab88058401de7688a5f5b6aec36312b1071b196
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-17 10:25:08 +0530

    MODIFIED : Create getNewUser to Take Input from User

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,33 +1,98 @@
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBookSystem
 {
-    Scanner sc = new Scanner(System.in);
+    ContactPersonRepo contactPersonRepo = new ContactPersonRepo();
+    Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        AddressBookSystem addressBookSystem = new AddressBookSystem();
+        int choice, exit = 5;
 
-        UserInterface ui = new UserInterface();
-        int choice = ui.ShowMainMenu();
+        do {
+            choice = userInterface.ShowMainMenu();
+            addressBookSystem.handlerUserSelection(choice);
+        } while (choice != exit);
 
-        AddressBookSystem abs = new AddressBookSystem();
-        abs.handlerUserSelection(choice);
 
     }
 
     public void handlerUserSelection(int choice)
     {
+        UserInterface userInterface = new UserInterface();
         switch (choice)
         {
             case 1:
+                    addContactPerson();
                 break;
             case 2:
+                List lstContactRepo = contactPersonRepo.printAllData();
+                userInterface.printAllData(lstContactRepo);
                 break;
             case 3:
+                break;
+            case 4:
+                break;
+            case 5:
                 break;
             default:
                     System.out.println("Wrong Choice ..!");
                 break;
         }
     }
+
+    public void addContactPerson()
+    {
+        ContactPerson contactPerson = new ContactPerson();
+
+        System.out.println("Enter The First Name :");
+        contactPerson.firstName = scanner.nextLine();
+
+        System.out.println("Enter The Last Name :");
+        contactPerson.lastName = scanner.nextLine();
+
+        System.out.println("Enter The Address :");
+        contactPerson.address = scanner.nextLine();
+
+        System.out.println("Enter The City :");
+        contactPerson.city = scanner.nextLine();
+
+        System.out.println("Enter The State :");
+        contactPerson.state = scanner.nextLine();
+
+        System.out.println("Enter The Zip :");
+        contactPerson.zip = scanner.nextLong();
+
+        System.out.println("Enter The Mobile Number :");
+        contactPerson.mobNo = scanner.nextLong();
+
+        System.out.println("1.MALE \n2.FEMALE \n3.OTHERS");
+        System.out.println("Select your Choice :");
+        int option = scanner.nextInt();
+
+        contactPerson.contInfo = new HashMap<>();
+
+        switch (option)
+        {
+            case 1:
+                    contactPerson.contInfo.put(ContactPerson.Gender.MALE,"MALE");
+                    break;
+            case 2:
+                    contactPerson.contInfo.put(ContactPerson.Gender.FEMALE,"FEMALE");
+                    break;
+            case 3:
+                    contactPerson.contInfo.put(ContactPerson.Gender.OTHERS,"OTHERS");
+                    break;
+            default:
+                    System.out.println("Wrong Choice..!");
+                    break;
+        }
+
+        contactPersonRepo.addContactPerson(contactPerson);
+    }
 }
+
+
