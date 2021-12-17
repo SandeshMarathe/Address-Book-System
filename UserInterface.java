fc4c7de102502ce96c66128161e0c2cefbaaa7bf
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-17 10:21:32 +0530

    MODIFIED : Show Menu to User & Loop for List

--- UserInterface.java
+++ UserInterface.java
@@ -1,16 +1,25 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInterface
 {
     public int ShowMainMenu()
     {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1.Add New Contact \n2.Edit Contact \n3.Delete Contact");
+        System.out.println("1.Add New Contact \n2.Print Contact \n3.Edit Contact \n4.Delete Contact \n5.Exit");
 
         System.out.println("Enter Your Choice :");
-        int choice = sc.nextInt();
+        int choice = scanner.nextInt();
 
         return choice;
+    }
+
+    public void printAllData(List lstContactRepo)
+    {
+        for (int i = 0; i < lstContactRepo.size(); i++)
+        {
+            System.out.println(lstContactRepo.get(i));
+        }
     }
 }
