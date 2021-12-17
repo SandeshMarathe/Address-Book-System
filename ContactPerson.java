50c3285b22a2d8f60b1542257107d0c570e7b91a
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-17 10:23:02 +0530

    MODIFIED : Declaration of Variables & ToString Method

--- ContactPerson.java
+++ ContactPerson.java
@@ -11,4 +11,18 @@ public class ContactPerson
     long zip, mobNo;
 
     Map<Gender,String> contInfo;
+
+    @Override
+    public String toString() {
+        return "ContactPerson{" +
+                "firstName='" + firstName + '\n' +
+                ", lastName='" + lastName + '\n' +
+                ", address='" + address + '\n' +
+                ", city='" + city + '\n' +
+                ", state='" + state + '\n' +
+                ", zip=" + zip + '\n' +
+                ", mobNo=" + mobNo + '\n' +
+                ", Gender=" + contInfo + '\n' +
+                '}';
+    }
 }
